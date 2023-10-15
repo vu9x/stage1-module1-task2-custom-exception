ea6665995da17ded8f0cc7d47b9a8516e6fd83b1
Vu <nguyentuanvu9x@gmail.com>
2023-10-15 13:12:43 +0300

    Adding: StudentException.class. Updating: StudentManager.class

---
+++ src/main/java/com/epam/mjc/StudentException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentException extends IllegalArgumentException {
+    public StudentException(String msg){
+        super(msg);
+    }
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,10 +1,5 @@
 package com.epam.mjc;
 
-class StudentException extends IllegalArgumentException {
-  public StudentException(String msg){
-    super(msg);
-  }
-}
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
