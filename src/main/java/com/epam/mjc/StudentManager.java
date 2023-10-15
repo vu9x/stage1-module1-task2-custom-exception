c4e9a266247d8d91173773eabb8e1131abe21a83
Vu <nguyentuanvu9x@gmail.com>
2023-10-15 12:58:37 +0300

    Updating: StudentManager.class, have moved StudentException test to the find method

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,32 +5,31 @@ class StudentException extends IllegalArgumentException {
     super(msg);
   }
 }
-
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-
+      try {
+          if(Student.getValueOf(studentID) == null){
+              throw new StudentException(String.format("Could not find student with ID %s", studentID));
+          }
+      } catch (StudentException ex){
+          System.out.println(ex.getMessage());
+      }
+      finally {
+          return Student.getValueOf(studentID);
+      }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      try {
         Student student = manager.find(IDs[i]);
-
-        if(student == null){
-          throw new StudentException(String.format("Could not find student with ID %s", IDs[i]));
-        } else {
-          System.out.println("Student name " + student.getName());
+        if(student != null) {
+            System.out.println("Student name " + student.getName());
         }
       }
-      catch(StudentException ex) {
-        System.out.println(ex.getMessage());
-      }
     }
-  }
 }
